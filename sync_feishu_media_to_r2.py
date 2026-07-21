222d22ba82da9d69fed1a78c744973cd9af3fb1b
cuiyisan-debug <cuiyisan-debug@users.noreply.github.com>
2026-07-21 21:15:33 +0800

    fix: configure r2 s3 signing

--- sync_feishu_media_to_r2.py
+++ sync_feishu_media_to_r2.py
@@ -20,6 +20,7 @@ from urllib.parse import urlparse
 
 import boto3
 import requests
+from botocore.config import Config
 from botocore.exceptions import ClientError
 
 
@@ -148,6 +149,7 @@ def main() -> None:
         aws_access_key_id=access_key_id,
         aws_secret_access_key=secret_access_key,
         region_name="auto",
+        config=Config(signature_version="s3v4", s3={"addressing_style": "path"}),
     )
     manifest = load_manifest(public_base_url)
     objects: dict[str, Any] = manifest.setdefault("objects", {})
